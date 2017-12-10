042791a0feb2573bd4d100fbcafe2884a2e307c9
Yoshua Wuyts <yoshuawuyts@gmail.com>
2017-12-11 01:10:22 +0900

    .

---
+++ index.js
@@ -0,0 +1,55 @@
+var assert = require('assert')
+
+function createScheduler () {
+  var hasWindow = typeof window !== 'undefined'
+  var scheduler
+  if (hasWindow) {
+    if (!window._nanoScheduler) window._nanoScheduler = new NanoScheduler(true)
+    scheduler = window._nanoScheduler
+  } else {
+    scheduler = new NanoScheduler()
+  }
+  return scheduler
+}
+
+function NanoScheduler (hasWindow) {
+  this.hasWindow = hasWindow
+  this.hasIdle = this.hasWindow && window.requestIdleCallback
+  this.method = this.hasIdle ? window.requestIdleCallback.bind(window) : this.setTimeout
+  this.scheduled = false
+  this.stack = []
+}
+
+NanoScheduler.prototype.push = function (cb) {
+  assert.equal(typeof cb, 'function', 'nanoscheduler.push: cb should be type function')
+  if (!this.hasWindow) return
+
+  this.stack.push(cb)
+  this.schedule()
+}
+
+NanoScheduler.prototype.schedule = function () {
+  if (this.scheduled) return
+
+  this.scheduled = true
+  var self = this
+  this.method(function (idleDeadline) {
+    var cb
+    while (self.stack.length && idleDeadline.timeRemaining() > 0) {
+      cb = self.stack.shift()
+      cb(idleDeadline)
+    }
+    self.scheduled = false
+    if (self.stack.length) self.schedule()
+  })
+}
+
+NanoScheduler.prototype.setTimeout = function (cb) {
+  setTimeout(cb, 0, {
+    timeRemaining: function () {
+      return 1
+    }
+  })
+}
+
+module.exports = createScheduler
